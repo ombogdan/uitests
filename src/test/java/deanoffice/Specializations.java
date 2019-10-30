0a37100427cec1bf61a1a4a6eef78c49220be542
ombogdan <omelcenko44@gmail.com>
2019-10-30 18:04:44 +0200

    Add DeleteCompetences test

--- src/test/java/deanoffice/Specializations.java
+++ src/test/java/deanoffice/Specializations.java
@@ -37,13 +37,12 @@ public class Specializations extends Login{
     }
 
     @Test
-    public void CreateCurrentCompetences() throws InterruptedException {
+    public void CreateNewCompetences() throws InterruptedException {
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
         GoTo goTo = new GoTo(driver);
         Login();
         goTo.Specializations();
-        //Вибрав спеціалізацію натиснувши на checkbox
         driver.findElement(By.cssSelector("#app > div > specialization > div > div > div > div > div:nth-child(2) > button.btn.btn-success.mr-1")).click();
         driver.findElement(By.xpath("//*[@id=\"name\"]")).sendKeys("Autotest");
         new Select (driver.findElement(By.id("specialityId"))).selectByIndex(6);
@@ -65,14 +64,12 @@ public class Specializations extends Login{
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/specialization/div/div/div/ag-grid-angular/div/div[1]/div/div[3]/div[2]/div/div/div[1]/div[1]/span/span[1]/span[2]")).click();
         //Вибрав групу натиснувши на checkbox
         goTo.editSpecializations();
-        WebElement q = driver.findElement(By.cssSelector("#name"));
-        WebDriverWait wait = new WebDriverWait(driver, 4);
-        wait.until(ExpectedConditions.visibilityOf(q));
-        wait.until(ExpectedConditions.elementToBeClickable(q));
-        q.sendKeys("Autotest");
-        //Ввів значення для нової назви спеціалізації
+        new Select (driver.findElement(By.id("degreeId"))).selectByIndex(1);
+
+        driver.findElement(By.xpath("//*[@id=\"nameEng\"]")).sendKeys(Keys.PAGE_DOWN);
         Thread.sleep(2000);
-        String expected= driver.findElement(By.xpath("//*[@id=\"app\"]/div/specialization/div/div/div/ag-grid-angular/div/div[1]/div/div[3]/div[2]/div/div/div[1]/div[1]/span/span[2]")).getText();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/specialization/update-specialization/modal-wrapper/div/div/div/div[3]/button[1]")).click();
+        String expected = driver.findElement(By.xpath("//*[@id=\"app\"]/div/specialization/div/div/div/ag-grid-angular/div/div[1]/div/div[3]/div[2]/div/div/div[1]/div[4]")).getText();
         //Знайшов назву групи
         assertThat("Інженерія програмного забезпечення", containsString(expected));
 
@@ -94,6 +91,37 @@ public class Specializations extends Login{
 //        assertThat("Інженерія програмного забезпечення", containsString(expected));
 
     }
+
+    @Test
+    public void CreateAndChooseAQualification() throws InterruptedException {
+        GoTo goTo = new GoTo(driver);
+        Login();
+        goTo.Specializations();
+        WebElement qwer = driver.findElement(By.xpath("//*[@id=\"app\"]/div/specialization/div/div/div/ag-grid-angular/div/div[1]/div/div[3]/div[2]/div/div/div[1]/div[1]/span/span[1]/span[2]"));
+        //Вибрав спеціалізацію натиснувши на checkbox
+        qwer.click();
+        goTo.editSpecializations();
+
+        driver.findElement(By.xpath("//*[@id=\"specialization-form-tabset\"]/ul/li[5]/a")).click();
+
+//        assertThat("Інженерія програмного забезпечення", containsString(expected));
+
+    }
+
+    @Test
+    public void DeleteCompetences() throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        GoTo goTo = new GoTo(driver);
+        Login();
+        goTo.Specializations();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/specialization/div/div/div/ag-grid-angular/div/div[1]/div/div[3]/div[2]/div/div/div[1]/div[1]/span/span[1]/span[2]")).click();
+        //Вибрав спеціалізацію натиснувши на checkbox
+        driver.findElement(By.cssSelector("#app > div > specialization > div > div > div > div > div:nth-child(2) > button.btn.btn-danger")).click();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/specialization/delete-specialization/modal-wrapper/div/div/div/div[3]/button[1]")).click();
+
+
+    }
+
 
 
 }
